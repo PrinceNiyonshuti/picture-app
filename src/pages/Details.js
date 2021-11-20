bd99ca6dc16633457865689b021b167156d80009
PrinceNiyonshuti <npprince47@gmail.com>
2021-11-20 18:19:48 +0200

    added rotuing to details page

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,11 +1,37 @@
-import React from 'react'
+/** @format */
+
+import React from "react";
+import { Link, useParams } from "react-router-dom";
 
 function Details() {
-    return (
-        <div>
-            
-        </div>
-    )
+    const { id } = useParams();
+	return (
+		<div className="ui container" style={{ marginTop: "15px" }}>
+			<Link to="/">
+				<i class="arrow left icon"></i> Go Back
+			</Link>
+			<div className="ui card">
+				<div className="image">
+					<img src="/images/avatar2/large/kristy.png" />
+				</div>
+				<div className="content">
+					<a className="header">Kristy</a>
+					<div className="meta">
+						<span className="date">Joined in 2013</span>
+					</div>
+					<div className="description">
+						Kristy is an art director living in New York.
+					</div>
+				</div>
+				<div className="extra content">
+					<a>
+						<i className="user icon"></i>
+						22 Friends
+					</a>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default Details
+export default Details;
