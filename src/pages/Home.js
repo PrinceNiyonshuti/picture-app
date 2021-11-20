f6f0274631ca6eb52c8ef9e54ba9d4209509dc8b
PrinceNiyonshuti <npprince47@gmail.com>
2021-11-20 20:07:12 +0200

    added icon

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,31 @@
 /** @format */
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SearchBar from "../components/SearchBar";
 import ImageList from "../components/ImageList";
 import Unsplash from "../api/Unsplash";
+import Pagination from "../components/Pagination";
 
 function Home() {
 	const [images, setImages] = useState();
+	const [totalPhotos, setTotalPhotos] = useState(0);
+	const [perPage, setPerPage] = useState(30);
+	const [page, setPage] = useState(5);
+	const [currentPage, setCurrentPage] = useState(1);
 
-	const onSearchSubmit = async (term) => {
-		const response = await Unsplash.get(
-			"search/photos?page=5&per_page=30&w=3000&fit=max&",
-			{
-				params: {
-					query: term,
-				},
-			}
-		);
+	const onSearchSubmit = async (term, page) => {
+		const response = await Unsplash.get("search/photos", {
+			params: {
+				query: term,
+				page,
+				per_page: perPage,
+			},
+		});
 		setImages(response.data.results);
+		setTotalPhotos(parseInt(response.headers["x-total"]));
+		setCurrentPage(page);
 	};
+
 	return (
 		<div className="ui container" style={{ marginTop: "15px" }}>
 			<SearchBar onSearchSubmit={onSearchSubmit} />
