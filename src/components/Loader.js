677565769bf3d936c1c8a57bba675ddc56ba7fba
PrinceNiyonshuti <npprince47@gmail.com>
2021-11-24 15:48:54 +0200

    added loader

---
+++ src/components/Loader.js
@@ -0,0 +1,16 @@
+/** @format */
+
+import React from "react";
+
+function Loader() {
+	return (
+		<div className="lds-ellipsis">
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+		</div>
+	);
+}
+
+export default Loader;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,15 +4,11 @@ import React, { useState, useEffect } from "react";
 import SearchBar from "../components/SearchBar";
 import ImageList from "../components/ImageList";
 import Unsplash from "../api/Unsplash";
-import Pagination from "../components/Pagination";
+import Loader from "../components/Loader";
 
 function Home() {
 	const [images, setImages] = useState();
-	const [totalPhotos, setTotalPhotos] = useState(0);
 	const [perPage, setPerPage] = useState(30);
-	const [page, setPage] = useState(5);
-	const [currentPage, setCurrentPage] = useState(1);
-
 	const onSearchSubmit = async (term, page) => {
 		const response = await Unsplash.get("search/photos", {
 			params: {
@@ -22,13 +18,12 @@ function Home() {
 			},
 		});
 		setImages(response.data.results);
-		setTotalPhotos(parseInt(response.headers["x-total"]));
-		setCurrentPage(page);
 	};
 
 	return (
 		<div className="ui container" style={{ marginTop: "15px" }}>
 			<SearchBar onSearchSubmit={onSearchSubmit} />
+			<Loader />
 			{images && <ImageList images={images} />}
 		</div>
 	);
