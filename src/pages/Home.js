f1c379020d3dd4b12c98318b28a8bddfa3b8ded3
PrinceNiyonshuti <npprince47@gmail.com>
2021-11-24 21:26:46 +0200

    added infinite scroll without test

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,26 +5,48 @@ import SearchBar from "../components/SearchBar";
 import ImageList from "../components/ImageList";
 import Unsplash from "../api/Unsplash";
 import Loader from "../components/Loader";
+import axios from "axios";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 function Home() {
-	const [images, setImages] = useState();
-	const [perPage, setPerPage] = useState(30);
+	const [images, setImages] = useState([]);
+
 	const onSearchSubmit = async (term, page) => {
 		const response = await Unsplash.get("search/photos", {
 			params: {
 				query: term,
 				page,
-				per_page: perPage,
+				per_page: 30,
 			},
 		});
 		setImages(response.data.results);
 	};
 
+	useEffect(() => {
+		fetchImages();
+	});
+
+	const fetchImages = () => {
+		const apiRoot = "https://api.unsplash.com";
+		const accessKey = "CZl0NjZ8Wkzj4DCy8TuO7bYe5_EZfZN9ascpEgOZCeE";
+
+		axios
+			.get(`${apiRoot}/photos/random?client_id=${accessKey}&count=5`)
+			.then((res) => setImages([...images, ...res.data]));
+	};
+
 	return (
 		<div className="ui container" style={{ marginTop: "15px" }}>
 			<SearchBar onSearchSubmit={onSearchSubmit} />
-			<Loader />
-			{images && <ImageList images={images} />}
+			<InfiniteScroll
+				dataLength={images.length}
+				next={fetchImages}
+				hasMore={true}
+				loader={<Loader />}>
+				{images && <ImageList images={images} />}
+			</InfiniteScroll>
+
+			{/* {images && <ImageList images={images} />} */}
 		</div>
 	);
 }
